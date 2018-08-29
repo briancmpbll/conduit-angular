549e6fbb972095a63402e794b6f7b273712f388d
Brian Campbell <briancmpbll@gmail.com>
2018-08-29 12:26:37 -0700

    Add api interceptor

---
+++ src/app/core/api.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      url: `${environment.api_url}/${request.url}`,
+      setHeaders: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    });
+
+    return next.handle(request);
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiInterceptor } from './api.interceptor';
 
 @NgModule({
   imports: [
     SharedModule
   ],
   declarations: [FooterComponent, HeaderComponent],
-  exports: [FooterComponent, HeaderComponent]
+  exports: [FooterComponent, HeaderComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule { }
