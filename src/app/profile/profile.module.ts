1a40d385d42b58debd20fc5d50e8a234da0461e5
Brian Campbell <briancmpbll@gmail.com>
2018-09-07 18:43:37 -0700

    Create the profile component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,12 +1,25 @@
+import { RouterModule } from '@angular/router';
 import { ProfileResolverGuard } from './profile-resolver.guard';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile/profile.component';
+
+const profileRouting = RouterModule.forChild([
+  {
+    path: 'profile/:username',
+    component: ProfileComponent,
+    resolve: {
+      profile: ProfileResolverGuard
+    }
+  }
+]);
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    profileRouting
   ],
-  declarations: [],
+  declarations: [ProfileComponent],
   providers: [
     ProfileResolverGuard
   ]
---
+++ src/app/profile/profile/profile.component.ts
@@ -0,0 +1,48 @@
+import { User } from './../../core/models/user.model';
+import { Profile } from './../../core/models/profile.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../core/services/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  profile?: Profile;
+  currentUser?: User;
+  isUser = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) { }
+
+  ngOnInit() {
+    this.route.data.subscribe((data) => {
+      this.profile = data.profile;
+      this.checkIsUser();
+    });
+
+    this.userService.currentUser.subscribe(userData => {
+      this.currentUser = userData;
+      this.checkIsUser();
+    });
+  }
+
+  onToggleFollowing(following: boolean) {
+    if (this.profile !== undefined) {
+      this.profile.following = following;
+    }
+  }
+
+  private checkIsUser() {
+    if (this.currentUser === undefined || this.profile === undefined) {
+      this.isUser = false;
+    } else {
+      this.isUser = this.currentUser.username === this.profile.username;
+    }
+  }
+
+}
