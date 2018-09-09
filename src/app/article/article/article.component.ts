783c668f2451a95d0497c060d32d743fa1400d12
Brian Campbell <briancmpbll@gmail.com>
2018-09-09 13:45:24 -0700

    Add logic to article component

--- src/app/article/article/article.component.ts
+++ src/app/article/article/article.component.ts
@@ -1,4 +1,10 @@
+import { ArticleService } from '../article.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Article } from './../../core/models/article.model';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../core/models/user.model';
+import { UserService } from '../../core/services/user.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +12,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  article: Article = new Article;
+  currentUser: User = new User;
+  canModify = false;
+  isSubmitting = false;
+  isDeleting = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService,
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.route.data.subscribe(data => this.article = data.article);
+
+    this.userService.currentUser.subscribe((userData: User) => {
+      this.currentUser = userData;
+
+      this.canModify = this.currentUser.username === this.article.author.username;
+    });
+  }
+
+  onToggleFavorite(favorited: boolean) {
+    this.article.favorited = favorited;
+
+    if (favorited) {
+      this.article.favoritesCount++;
+    } else {
+      this.article.favoritesCount--;
+    }
+  }
+
+  onToggleFollowing(following: boolean) {
+    this.article.author.following = following;
+  }
+
+  deleteArticle() {
+    this.isDeleting = true;
+
+    this.articleService.destroy(this.article.slug)
+    .pipe(
+      finalize(() => this.isDeleting = false)
+    )
+    .subscribe(success => this.router.navigateByUrl('/'));
   }
 
 }
